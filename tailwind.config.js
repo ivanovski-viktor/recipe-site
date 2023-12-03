a44689c7017fe4e4feab7f86cbd28f0383b22afb
vikmeisterr <viktor.ivanovski11@gmail.com>
2023-12-03 16:15:15 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  purge: ["./dist/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
